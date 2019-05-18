d1042f3f069e5d32cecefec0a666b999099b47bd
Brady Niemitalo Woods <49171897+BDPNC101@users.noreply.github.com>
2019-05-18 16:03:12 -0600

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,10 +1,16 @@
-
-
 def alphabet_position(letter):
-    #Add your code here
-    return
-
+    # receives a letter and returns the 0-based numerical 
+    # position of that letter within the alphabet.
+    ordinal_of_letter = ord(letter)
+    if 90 >= ordinal_of_letter >= 65:
+        return ordinal_of_letter - 65 
+    return ordinal_of_letter - 97
 
 def rotate_character(char, rot):
-    #Add your code here
-    return
+    if char.isalpha():
+        char_value = alphabet_position(char)
+        new_char_value = (char_value + int(rot)) % 26
+        if 90 >= ord(char) >= 65:
+            return chr(new_char_value + 65)
+        return chr(new_char_value + 97)
+    return char
